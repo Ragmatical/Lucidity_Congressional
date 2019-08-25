80af85d65d11d365323782e32be692e81a3c9e84
Juno Kim <junokimzone@gmail.com>
2019-08-25 10:01:43 -0700

    Create rewards.js

---
+++ Schema Stuff/rewards.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var model = mongoose.model('userdata',new mongoose.Schema({
+    tokenCount: String
+    , tokenValue: String
+    , 
+}));
+
+exports.getModel = function() {
+    return model
+}
