4071ce799640593173a08d3e505a1d4c0883834f
Juno Kim <junokimzone@gmail.com>
2019-09-22 09:16:41 -0700

    Update users.js

--- Schemas/users.js
+++ Schemas/users.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-<<<<<<< HEAD
+
 var model = mongoose.model('userdata',new mongoose.Schema({
     username: { type: String, required: true, unuque: true, lowercase: true }
     , premium: {type: Boolean, default: false}
@@ -10,18 +10,6 @@ var model = mongoose.model('userdata',new mongoose.Schema({
     , stripeCustomerId: {type: String, required: true}
     , dateCreated: {type: Date, required: true}
     , dateUpdated: {type: Date, required: true}
-=======
-var model = mongoose.model('users',new mongoose.Schema({
-    username: {type:String, required: true, unique: true, lowercase: true}
-    , premium: {type:Boolean, default:false}
-    , password: {type:String, required:true}
-    , salt: String
-    , email: {type:String, required: true}
-    , phonenumber: {type:String, required:true}
-    , stripeCustomerId: {type:String, required:true}
-    , dateCreated: {type:Date, required:true}
-    , dateUpdated: {type:Date, required:true}
->>>>>>> 7e8584e9b1b52623917966048b9e99d89b76729b
     }
     , {usePushEach: true, collection: 'users'}
 ));
